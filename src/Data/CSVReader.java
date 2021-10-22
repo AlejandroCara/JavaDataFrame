1980350b2c8257da7210c857f2d61b5c0b2dd16d
AlejandroCara <alejandro.carayol@gmail.com>
2021-10-22 16:01:06 +0200

    Refactor CSVReaderFactory -> CSVReader

---
+++ src/Data/CSVReader.java
@@ -0,0 +1,11 @@
+package Data;
+
+public class CSVReader {
+
+    //Class to read and save data from csv
+    public CSVReader(String path){
+
+    }
+
+
+}
--- src/Data/FileReaderFactory.java
+++ src/Data/FileReaderFactory.java
@@ -2,6 +2,7 @@ package Data;
 
 public class FileReaderFactory {
 
+    //Factory to read 3 types of files CSV, JSON, TXT
     public FileReaderFactory(String path){
 
     }
